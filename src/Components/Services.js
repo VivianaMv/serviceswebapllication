b70f2a6b0d5f7bad1748faf044339abd50184557
VivianaMv <VIVIANAMONTOYA14@GMAIL.COM>
2024-08-25 14:14:27 -0400

    Servicespage

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -15,36 +15,50 @@ const Services = () => {
                     className='logo-cleaning2'
                     src={require('../images/house-cleaning.jpg')}
                     alt='cleaning logo' />
-                <div className='CleaningDescrip'>
+                <div className='Descrip'>
                     <p > Home cleaning service: We help you keep every space in your house looking new. We have qualified equipment and we use quality, environmentally friendly products. We respect your spaces and privacy, so we carefully follow your comments and/or instructions regarding care and details that you consider we should focus on in each task.</p>
 
                     <p >* Cost of service per hour $21 dollars including taxes.</p>
                     <p >* Cost of service per hour + cleaning products $26 dollars including taxes.</p>
                     <p > *Cash payment method once the service is finished.</p>
+                    <button className='btnBook'>Book services</button>
                 </div>
+
             </div>
             <div>
                 <img
-                    className='logo-gardening'
+                    className='logo-gardening2'
                     src={require('../images/gardening.jpg')}
                     alt='gardening logo' />
+
+                <div className='Descrip'>
+                    <p > Home cleaning service: We help you keep every space in your house looking new. We have qualified equipment and we use quality, environmentally friendly products. We respect your spaces and privacy, so we carefully follow your comments and/or instructions regarding care and details that you consider we should focus on in each task.</p>
+
+                    <p >* Cost of service per hour $21 dollars including taxes.</p>
+                    <p >* Cost of service per hour + cleaning products $26 dollars including taxes.</p>
+                    <p > *Cash payment method once the service is finished.</p>
+                    <button className='btnBook'>Book services</button>
+                </div>
+
             </div>
             <div>
                 <img
-                    className='logo-painting'
+                    className='logo-painting2'
                     src={require('../images/painting.jpg')}
                     alt='painting logo' /><br></br><br></br><br></br><br></br>
+                <div className='Descrip'>
+                    <p > Home cleaning service: We help you keep every space in your house looking new. We have qualified equipment and we use quality, environmentally friendly products. We respect your spaces and privacy, so we carefully follow your comments and/or instructions regarding care and details that you consider we should focus on in each task.</p>
 
+                    <p >* Cost of service per hour $21 dollars including taxes.</p>
+                    <p >* Cost of service per hour + cleaning products $26 dollars including taxes.</p>
+                    <p > *Cash payment method once the service is finished.</p>
+                    <button className='btnBook'>Book services</button>
+                    <br></br><br></br><br></br>
+
+                </div>
             </div>
-            <p> Home cleaning service: We help you keep every space in your house looking new. We have qualified equipment and we use quality, environmentally friendly products. We respect your spaces and privacy, so we carefully follow your comments and/or instructions regarding care and details that you consider we should focus on in each task.</p>
-
-            <p>* Cost of service per hour $21 dollars including taxes.</p>
-            <p>* Cost of service per hour + cleaning products $26 dollars including taxes.</p>
-            <p>*Cash payment method once the service is finished.</p>
-            <div>
 
 
-            </div>
             <a className='Privacy' href='Privacy.js'>Privacy</a>
 
             <a className='Privacy' href='TermCon.js' target='_blanck'>Terms and conditions</a>
